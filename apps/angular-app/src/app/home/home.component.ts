2661f9a93bc0c3abb3a7985a86daeb8e8484e998
alibeaj.flavio <alibeaj.flavio@gmail.com>
2025-02-02 00:39:39 +0100

    converted app to monorepo

---
+++ apps/angular-app/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [ToolbarComponent, SidenavComponent],
+  styles: [
+    `
+      .main-div {
+        // display: flex;
+        // flex-direction: column;
+        height: 100%;
+        // height: 100vh;
+        overflow: hidden;
+      }
+
+      /*
+      app-toolbar {
+        flex: 0 0 auto;
+      }
+      */
+
+      // app-sidenav {
+      //   /*
+      //   flex: 1 1 auto;
+      //   */
+      //   flex: auto;
+      //   overflow: auto;
+      // }
+    `,
+  ],
+  template: `
+    <div class="main-div">
+      <app-toolbar />
+      <app-sidenav />
+    </div>
+  `,
+})
+export class HomeComponent {}
