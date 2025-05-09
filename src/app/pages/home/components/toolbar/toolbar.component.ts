23573098bd87e72233404549a44d75db6ca9551f
Flavio Alibeaj <flavio@dev.al>
2025-05-09 15:28:38 +0200

    feat: integrate toggle sidenav directive into toolbar component

--- src/app/pages/home/components/toolbar/toolbar.component.ts
+++ src/app/pages/home/components/toolbar/toolbar.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { SidenavService } from '../../services/sidenav.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -14,6 +13,7 @@ import { InformationDialogComponent } from '../../../../shared/components/inform
 import { IFormResponse } from '../../../../shared/model/i-form-response.interface';
 import { IInformationDialogData } from '../../../../shared/model/i-information-dialog-data';
 import { ThemeSwitchComponent } from '../../../../shared/components/theme-switch/theme-switch.component';
+import { ToggleSidenavDirective } from '../../../../shared/directives/toggle-sidenav.directive';
 
 @Component({
   selector: 'app-toolbar',
@@ -26,6 +26,7 @@ import { ThemeSwitchComponent } from '../../../../shared/components/theme-switch
     MatTooltipModule,
     LowerCasePipe,
     ThemeSwitchComponent,
+    ToggleSidenavDirective,
   ],
   template: `
     <mat-toolbar>
@@ -33,7 +34,7 @@ import { ThemeSwitchComponent } from '../../../../shared/components/theme-switch
         mat-icon-button
         aria-label="Icon-button with menu icon"
         onclick="this.blur()"
-        (click)="sidenavService.toggleSidenav()"
+        toggleSidenav
       >
         <mat-icon>menu</mat-icon>
       </button>
@@ -89,7 +90,6 @@ import { ThemeSwitchComponent } from '../../../../shared/components/theme-switch
   `,
 })
 export class ToolbarComponent {
-  readonly sidenavService = inject(SidenavService);
   readonly translateService = inject(TranslateService);
   readonly #authService = inject(AuthService);
   readonly #matDialog = inject(MatDialog);
