0bacde5b289a390f13552d0181bb1fffca86f7e3
Flavio Alibeaj <flavio@dev.al>
2025-08-05 17:05:14 +0200

    todo learn

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// TODO learn auth0
+// TODO learn stripe
+// TODO learn ssr
+// TODO learn angular AI integration // https://github.com/angular/examples
+// https://www.angulartraining.com/daily-newsletter/
+// https://certificates.dev/angular#top --- username: tedi@dev.al --- password: 6:VZK'@mk4NKx4r
+// https://angular.love/
+// https://courses.angulartraining.com/courses
+// https://blog.angular-university.io/
+// TODO learn PWA // https://web.dev/explore/progressive-web-apps
+// TODO learn defer blocks
